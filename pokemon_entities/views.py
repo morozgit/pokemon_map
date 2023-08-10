629d7a267cbcaa7fbc501b812980f9586fabe350
morozgit <morozec76@gmail.com>
2023-08-10 17:26:08 +0300

    show pokemons on mainpage map

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -15,6 +15,7 @@ DEFAULT_IMAGE_URL = (
 
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+    print(image_url)
     icon = folium.features.CustomIcon(
         image_url,
         icon_size=(50, 50),
@@ -31,35 +32,25 @@ def show_all_pokemons(request):
     with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
         pokemons = json.load(database)['pokemons']
 
+    print(PokemonEntity.objects.all())
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
         for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+            for pocemon_from_admin in PokemonEntity.objects.all():
+                pokemon_url = request.build_absolute_uri(pocemon_from_admin.pockemon.photo.url)
+                add_pokemon(
+                    folium_map, pokemon_entity['lat'],
+                    pokemon_entity['lon'],
+                    # pokemon['img_url']
+                    pokemon_url
+                )
 
-    # pokemons_on_page = []
-    # for pokemon in pokemons:
-    #     pokemons_on_page.append({
-    #         'pokemon_id': pokemon['pokemon_id'],
-    #         'img_url': pokemon['img_url'],
-    #         'title_ru': pokemon['title_ru'],
-    #     })
-
-    p = Pokemon.objects.all()
-    print(p)
-    url = request.build_absolute_uri(f'/media/{p[0].photo.name}')
-    # url = request.build_absolute_uri('/media/pokemon/bulbazavr.jpg')
-    
-    print('url', url)
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
-            'pokemon_id': p[0].id,
-            'img_url': url,
-            'title_ru': p[0].title,
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.photo.url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
