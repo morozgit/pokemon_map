8d2396772d8a6c0d8791f6d372acc70bc38b5107
morozgit <morozec76@gmail.com>
2023-08-14 16:33:01 +0300

    add optional fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
-    describe = models.TextField(max_length=1000)
-    photo = models.ImageField(upload_to='pokemon', null=True)
+    title_ru = models.CharField(max_length=200, default='')
+    title_en = models.CharField(max_length=200, default='', blank=True)
+    title_jp = models.CharField(max_length=200, default='', blank=True)
+    describe = models.TextField(max_length=1000, default='', blank=True)
+    photo = models.ImageField(upload_to='pokemon', null=False, blank=False)
     previous_evolution = models.ForeignKey('self',
                                            verbose_name='Из кого эволюционирует',
                                            null=True,
@@ -19,13 +19,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="entities",)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(default=0)
-    heath = models.IntegerField(default=100)
-    strength = models.IntegerField(default=0)
-    defense = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE,
+                                related_name="entities",
+                                null=False, blank=False)
+    latitude = models.FloatField(null=False, blank=False)
+    longitude = models.FloatField(null=False, blank=False)
+    appeared_at = models.DateTimeField(null=False, blank=False)
+    disappeared_at = models.DateTimeField(null=False, blank=False)
+    level = models.IntegerField(default=0, null=True, blank=True)
+    heath = models.IntegerField(default=100, null=True, blank=True)
+    strength = models.IntegerField(default=0, null=True, blank=True)
+    defense = models.IntegerField(default=0, null=True, blank=True)
+    stamina = models.IntegerField(default=0, null=True, blank=True)
